c2837e6bbcd230d7cb6636ac6efd2bd285efb8b2
ren4tus <20171240@imrlab.hoseo.edu>
2023-03-31 14:36:32 +0900

    First Commit

---
+++ src/main/java/com/kissco/ex/KisscoShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.kissco.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisscoShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisscoShoppingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kissco/ex/KisscoShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kissco.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KisscoShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
